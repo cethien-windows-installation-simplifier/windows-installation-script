a406856d952e6194f64874e9d0373ee7c154c81b
Cethien <60463918+Cethien@users.noreply.github.com>
2021-09-04 15:50:44 +0200

    Update install-main.bat

--- install-main.bat
+++ install-main.bat
@@ -1,40 +1,43 @@
 rem Script to install some Essential Software in windows
 
+rem Windows Terminal
+winget install -e --id Microsoft.WindowsTerminal
+
+rem Java
+winget install -e --id AdoptOpenJDK.OpenJDK.16
+
+rem 7Zip
+winget install -e --id 7zip.7zip
+
+rem VoiceMeeterPotato
+winget install -e --id VB-Audio.VoiceMeeterPotato
+
+rem Firefox
+winget install -e --id Mozilla.Firefox
+
+rem Drive File Stream
+winget install -e --id Google.DriveFileStream
+
+rem Thunderbid beta
+winget install -e --id Mozilla.ThunderbirdBeta
+
+rem Spotify
+winget install -e --id Spotify.Spotify
+
+rem Discord
+winget install -e --id Discord.Discord
+
+rem Adobe Acrobat Reader DC
+winget install -e --id Adobe.AdobeAcrobatReaderDC
+
+rem Bulk Rename Utility
+winget install -e --id TGRMNSoftware.BulkRenameUtility
+
 rem EarTrumpet
 winget install -e --id File-New-Project.EarTrumpet
 
 rem ModernFlyouts
 winget install -e --id ModernFlyouts.ModernFlyouts
 
-rem Windows Terminal
-winget install -e --id Microsoft.WindowsTerminal
-
-rem 7Zip
-winget install -e --id 7zip.7zip
-
-rem Firefox
-winget install -e --id Mozilla.Firefox
-
-rem Discord
-winget install -e --id Discord.Discord
-
-rem Spotify
-winget install -e --id Spotify.Spotify
-
-rem Drive File Stream
-winget install -e --id Google.DriveFileStream
-
-rem Adobe Acrobat Reader DC
-winget install -e --id Adobe.AdobeAcrobatReaderDC
-
-rem Java
-winget install -e --id AdoptOpenJDK.OpenJDK.16
-
-rem Thunderbid beta
-winget install -e --id Mozilla.ThunderbirdBeta
-
 rem Rainmeter
 winget install Rainmeter
-
-rem Bulk Rename Utility
-winget install -e --id TGRMNSoftware.BulkRenameUtility
