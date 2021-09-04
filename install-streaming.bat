b9a7c63678e1419b8cce63bbda1e2455738bb960
Cethien <60463918+Cethien@users.noreply.github.com>
2021-09-04 15:50:52 +0200

    Update install-streaming.bat

--- install-streaming.bat
+++ install-streaming.bat
@@ -2,6 +2,3 @@ rem Windows Script to install some stuff for internet fame
 
 rem OBS Studio
 winget install -e --id OBSProject.OBSStudio
-
-rem VoiceMeeterPotato
-winget install -e --id VB-Audio.VoiceMeeterPotato
