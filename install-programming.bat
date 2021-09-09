0f7d58cc1665df911cf27d1ada835a80b73b1227
Cethien <60463918+Cethien@users.noreply.github.com>
2021-09-09 13:52:18 +0200

    Update install-programming.bat

--- install-programming.bat
+++ install-programming.bat
@@ -1,4 +1,7 @@
 rem Windows Script to install my Alpha-Virgin Stuff
 
+rem KeepassXC
+winget install -e --id KeePassXCTeam.KeePassXC
+
 rem Visual Studio Code
 winget install -e --id Microsoft.VisualStudioCode
