90f1423fa599606592fe50ba3acd80ca36e4c23f
Cethien <60463918+Cethien@users.noreply.github.com>
2021-10-07 00:18:58 +0200

    Create intall-communication.bat

---
+++ intall-communication.bat
@@ -0,0 +1,10 @@
+rem Communication
+
+rem Discord
+winget install -e --id Discord.Discord
+
+rem Teamspeak
+winget install -e --id TeamSpeakSystems.TeamSpeakClient
+
+rem Whatsapp Tray
+start "" "https://github.com/D4koon/WhatsappTray/releases"
