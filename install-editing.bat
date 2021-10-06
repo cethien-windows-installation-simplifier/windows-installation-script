052729c0dbdf74ce8a48f5b9ba6418b2ebd49a08
Cethien <60463918+Cethien@users.noreply.github.com>
2021-10-07 00:13:30 +0200

    Create install-editing.bat

---
+++ install-editing.bat
@@ -0,0 +1,13 @@
+rem Windows Script to install some stuff for editing stuff
+
+rem paint.net
+start "" "https://www.getpaint.net/"
+
+rem inkscape
+winget install -e --id Inkscape.Inkscape
+
+rem gimp
+winget install -e --id GIMP.GIMP
+
+rem audacity
+winget install -e --id Audacity.Audacity
