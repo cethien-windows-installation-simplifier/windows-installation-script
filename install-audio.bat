91593ee2398111563711cba4dcc1e0db33f1bb37
Cethien <60463918+Cethien@users.noreply.github.com>
2021-10-07 03:11:44 +0200

    Update install-audio.bat

--- install-audio.bat
+++ install-audio.bat
@@ -7,7 +7,7 @@ rem equalizer apo
 start "" "https://sourceforge.net/projects/equalizerapo/"
 
 rem VoiceMeeterPotato
-winget install -e --id VB-Audio.VoiceMeeterPotato
+winget install -e --id VB-Audio.VoicemeeterPotato
 
 rem Virtual Audio Cable
 start "" "https://vb-audio.com/Cable/"
