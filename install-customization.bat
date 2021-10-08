013bda5a19c088ce5adf68f28709c26f587c0a66
Cethien <60463918+Cethien@users.noreply.github.com>
2021-10-08 15:00:21 +0200

    Update install-customization.bat

--- install-customization.bat
+++ install-customization.bat
@@ -2,3 +2,11 @@ rem Windows Script to install some stuff so my desktop looks pretty
 
 rem Rainmeter
 winget install -e --id Rainmeter.Rainmeter
+
+rem Spicetify
+Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.ps1" | Invoke-Expression
+spicetify
+spicetify config extensions webnowplaying.js
+spicetify config custom_apps new-releases
+spicetify config inject_css 0 replace_colors 0
+spicetify backup apply
