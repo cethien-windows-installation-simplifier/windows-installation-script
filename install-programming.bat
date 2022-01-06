b7ff6fb0b878738e64c44489af032867f5491351
Cethien <60463918+Cethien@users.noreply.github.com>
2022-01-06 19:21:00 +0100

    Update install-programming.bat

--- install-programming.bat
+++ install-programming.bat
@@ -1,16 +1,13 @@
 rem Windows Script to install my Alpha-Virgin Stuff
 
-rem Docker
-winget install -e --id Docker.DockerDesktop
-
-rem KeepassXC
-winget install -e --id KeePassXCTeam.KeePassXC
-
-rem Vim
-winget install -e --id vim.vim
+rem Fira Code Font (for VSCode)
+start "" "https://github.com/tonsky/FiraCode/releases"
 
 rem Visual Studio Code
 winget install -e --id Microsoft.VisualStudioCode -i
 
-rem Fira Code Font (for VSCode)
-start "" "https://github.com/tonsky/FiraCode/releases"
+rem KeepassXC
+winget install -e --id KeePassXCTeam.KeePassXC
+
+rem Docker
+winget install -e --id Docker.DockerDesktop
