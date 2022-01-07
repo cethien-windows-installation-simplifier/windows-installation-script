26a97a5911bab0a6476c599cfbc59e7bc273f842
Cethien <60463918+Cethien@users.noreply.github.com>
2022-01-07 18:12:54 +0100

    Update wsl-install-dev.sh

--- wsl-install-dev.sh
+++ wsl-install-dev.sh
@@ -2,7 +2,7 @@
 
 # installments for WSL2 Ubuntu
 
-# github cli
+# github cli (install line per line)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
