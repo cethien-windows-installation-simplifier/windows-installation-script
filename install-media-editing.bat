2b07b97cb87ea2e8d5bd6639c78fbc6696dcc841
Cethien <60463918+Cethien@users.noreply.github.com>
2022-01-08 15:13:50 +0100

    Update and rename install-editing.bat to install-media-editing.bat

--- install-media-editing.bat
+++ install-media-editing.bat
@@ -6,8 +6,8 @@ start "" "https://www.getpaint.net/"
 rem inkscape
 winget install -e --id Inkscape.Inkscape
 
-rem audacity
-winget install -e --id Audacity.Audacity
-
 rem gimp
 winget install -e --id GIMP.GIMP
+
+rem audacity
+winget install -e --id Audacity.Audacity
