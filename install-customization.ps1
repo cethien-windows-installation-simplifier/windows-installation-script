d6dbec036c834fd1aca6621a27fa983125ff1830
Cethien <borislaw.sotnikow@gmx.de>
2022-01-08 18:12:02 +0100

    added iCUE (corsair hardware config software)

--- install-customization.ps1
+++ install-customization.ps1
@@ -1,11 +1,15 @@
 # Windows Script to install some stuff so my desktop looks pretty
 
+# Corsair iCUE
+winget install -e --id Corsair.iCUE.4
+
 # Rainmeter
 winget install -e --id Rainmeter.Rainmeter
 
 # Spicetify
 Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.ps1" | Invoke-Expression
 
+# install spicetify expensions & apps
 spicetify
 spicetify config extensions webnowplaying.js
 spicetify config custom_apps new-releases
