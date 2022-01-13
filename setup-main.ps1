5653dfea7344f1c012e4ae6b576b78c4a5a6e1c2
borislaw.sotnikow <borislaw.sotnikow@gmx.de>
2022-01-13 14:47:37 +0100

    added Chrome & librnnoise

--- setup-main.ps1
+++ setup-main.ps1
@@ -1,9 +1,10 @@
 # Script to install my stuff
-$downloadLocation = $ENV:USERPROFILE + "\Downloads"
+$downloadLocation = $ENV:USERPROFILE + "/Downloads"
+$vstPath = $ENV:ProgramFiles + "/VSTPlugins"
 
 # FUNCTIONS
 # ====
-function getGithubRepoUrl($repo, $assetPattern) {
+function getGithubLatestReleaseUrl($repo, $assetPattern) {
     $releasesUri = "https://api.github.com/repos/$repo/releases/latest"
     $asset = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).assets | Where-Object name -like $assetPattern
     return $asset.browser_download_url
@@ -27,6 +28,8 @@ winget install -e --id Microsoft.PowerToys
 
 # APPS
 # ====
+# Google Chrome
+winget install -e --id Google.Chrome
 # Spotify
 winget install -e --id Spotify.Spotify
 # Discord
@@ -42,19 +45,19 @@ winget install -e --id TeamSpeakSystems.TeamSpeakClient
 # WhatsApp
 winget install -e --id WhatsApp.WhatsApp
 # WhatsApp Tray
-$uri = getGithubRepoUrl 'D4koon/WhatsappTray' 'WhatsappTray*.exe'
+$uri = getGithubLatestReleaseUrl 'D4koon/WhatsappTray' 'WhatsappTray*.exe'
 $file = 'InstallWhatsappTray-latest.exe'
-Invoke-WebRequest -Uri $uri -OutFile $downloadLocation\$file
+Invoke-WebRequest -Uri $uri -OutFile $downloadLocation/$file
 
-Invoke-Item $downloadLocation\$file
+Invoke-Item $downloadLocation/$file
 
 # EDITING
 # ====
 # paint.net
 $uri = "https://www.dotpdn.com/files/paint.net.4.3.7.install.anycpu.web.zip"
 $file = 'paintNET.zip'
-Invoke-WebRequest -Uri $uri -OutFile $downloadLocation\$file
-unzip $downloadLocation\$file -d 
+Invoke-WebRequest -Uri $uri -OutFile $downloadLocation/$file
+unzip $downloadLocation/$file -d 
 # inkscape
 winget install -e --id Inkscape.Inkscape
 # gimp
@@ -65,19 +68,18 @@ winget install -e --id Audacity.Audacity
 # MISC
 # ====
 # FiraCode
-$uri = getGithubRepoUrl 'tonsky/FiraCode' 'Fira_Code_v*.zip'
+$uri = getGithubLatestReleaseUrl 'tonsky/FiraCode' 'Fira_Code_v*.zip'
 $file = 'FiraCode-latest.zip'
-Invoke-WebRequest -Uri $uri -OutFile $downloadLocation\$file
-
-unzip $downloadLocation\$file "variable_ttf/*"
-./variable_ttf/FiraCode-VF.ttf
+Invoke-WebRequest -Uri $uri -OutFile $downloadLocation/$file
+unzip $downloadLocation/$file "variable_ttf/*"
+Invoke-Item ./variable_ttf/FiraCode-VF.ttf
 
 # CUSTOMIZATION
 # ====
 # Logitech G Hub
 $file = 'InstallLogitechGhub.exe'
-Invoke-WebRequest -Uri "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe" -OutFile $downloadLocation\$file
-Invoke-Item $downloadLocation\$file
+Invoke-WebRequest -Uri "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe" -OutFile $downloadLocation/$file
+Invoke-Item $downloadLocation/$file
 # Corsair iCUE
 winget install -e --id Corsair.iCUE.4
 # Rainmeter
@@ -87,19 +89,39 @@ Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/khanhas/sp
 
 # AUDIO
 # ====
+
 # EarTrumpet
 winget install -e --id File-New-Project.EarTrumpet
+
 # Equalizer APO
 $file = 'InstallEqualizerAPO-latest.exe'
-Invoke-WebRequest -UserAgent "Wget" -Uri "https://sourceforge.net/projects/equalizerapo/files/latest/download" -OutFile $downloadLocation\$file
-Invoke-Item $downloadLocation\$file
+Invoke-WebRequest -UserAgent "Wget" -Uri "https://sourceforge.net/projects/equalizerapo/files/latest/download" -OutFile $downloadLocation/$file
+Invoke-Item $downloadLocation/$file
+
+# Noise Suppresion VST
+$uri = getGithubLatestReleaseUrl 'werman/noise-suppression-for-voice' 'windows_rnnoise_bin_x64*.zip'
+$file = 'rnnoise-latest.zip'
+Invoke-WebRequest -Uri $uri -OutFile $downloadLocation/$file
+$location = "$downloadLocation/librnnoiseVST"
+$readmefile = "install-readme.txt"
+if (!(Test-Path $location)) {   
+    New-Item $location -ItemType Directory
+}
+if (!(Test-Path "$location/$readmefile")) {    
+    New-Item "$location/$readmefile" -ItemType File -Value "Install by dropping the .dll from bin/vst/ into $vstPath"
+}
+unzip "$downloadLocation/$file" "bin/vst/*" -d $location
+explorer.exe $location
+Invoke-Item "$location/$readmefile"
+
 # VoiceMeeter Potato
 winget install -e --id VB-Audio.VoicemeeterPotato
+
 # Virtual Audio Cable
 $file = 'VBCABLE_Driver_Pack.zip'
-Invoke-WebRequest -UserAgent "Wget" -Uri "https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip" -OutFile $downloadLocation\$file
-unzip $downloadLocation\$file -d ".\VBCABLE\"
-Invoke-Item $downloadLocation\VBCABLE\VBCABLE_Setup_x64.exe
+Invoke-WebRequest -UserAgent "Wget" -Uri "https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip" -OutFile $downloadLocation/$file
+unzip $downloadLocation/$file -d "./VBCABLE/"
+Invoke-Item $downloadLocation/VBCABLE/VBCABLE_Setup_x64.exe
 
 # PROGRAMMING
 # ====
@@ -114,10 +136,12 @@ winget install -e --id KeePassXCTeam.KeePassXC
 
 # GAMING
 # ====
+
 # Steam
 winget install -e --id Valve.Steam
 
 # STREAMING
 # ====
+
 # OBS Studio
 winget install -e --id OBSProject.OBSStudio
