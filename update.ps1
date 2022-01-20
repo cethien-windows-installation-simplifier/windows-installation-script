b663b1c2e8a70992d5511d135f0b556e12b3e0d6
Cethien <borislaw.sotnikow@gmx.de>
2022-01-20 17:23:17 +0100

    logging

--- update.ps1
+++ update.ps1
@@ -1,6 +1,7 @@
 Write-Host "Updating Winget files..."
 winget upgrade --all
-Write-Host "Updating done!"
+Write-Host "Updating winget done!"
 
 Write-Host "Updating Spictify"
 spicetify upgrade
+Write-Host "Updating Spictify done°"
