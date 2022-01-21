1bfc5a4290a9bf3152284e5860de3bd83738b4d3
Cethien <borislaw.sotnikow@gmx.de>
2022-01-21 13:36:45 +0100

    added script updater

--- update.ps1
+++ update.ps1
@@ -1,7 +1,14 @@
+Write-Host "Fetching newest Update script..."
+$startup = [Environment]::GetFolderPath("CommonStartup")
+Invoke-WebRequest `
+    -Uri "https://raw.githubusercontent.com/Cethien/windows-install-scripts/main/update.ps1" `
+    -OutFile "$startup/update.ps1"
+Write-Host "Newest Update installed!"
+
 Write-Host "Updating Winget files..."
 winget upgrade --all
-Write-Host "Updating winget done!"
+Write-Host "winget apps updated!"
 
 Write-Host "Updating Spictify..."
 spicetify upgrade
-Write-Host "Updating Spictify done!"
+Write-Host "Spictify updated!"
