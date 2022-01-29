5ea0f27698427443d5465d7449c73795883734a7
Cethien <borislaw.sotnikow@gmx.de>
2022-01-29 13:31:22 +0100

    changed how updating works

--- update.ps1
+++ update.ps1
@@ -1,10 +1,3 @@
-Write-Host "Fetching newest Update script..."
-$startup = [Environment]::GetFolderPath("CommonStartup")
-Invoke-WebRequest `
-    -Uri "https://raw.githubusercontent.com/Cethien/windows-install-scripts/main/update.ps1" `
-    -OutFile "$startup/update.ps1"
-Write-Host "Newest Update installed!"
-
 Write-Host "Updating Spictify..."
 spicetify upgrade
 Write-Host "Spictify updated!"
