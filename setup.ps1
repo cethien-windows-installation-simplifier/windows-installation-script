e7e7e7f06237bd9e35bb8809ad101e60b7eee666
borislaw.sotnikow <borislaw.sotnikow@kuhfussonline.com>
2022-03-08 11:16:59 +0100

    cleanup, path to local for downloads

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,9 @@
-$json = "C:/Users/borislaw.sotnikow/source/repos/WindowsInstallationSimplifier.DataGenerator/WindowsInstallationSimplifier.DataGenerator/bin/Debug/net6.0-windows/.out/data.json"
+$json = "./data.json"
 $data = Get-Content $json | ConvertFrom-Json
 
 # select installation profile
-$title = "Hi!"
-$msg = "Before we start, select the profile you want to use:"
+$title = "Hi " + $data.user_name + "!"
+$msg = "Select the profile you want to use:"
 $options = $(
     foreach ($profile in $data.install_profiles) {
         "&" + $profile.name
@@ -16,19 +16,21 @@ do {
 } until ($response -in 0..($data.install_profiles.Count - 1))
 
 $selectedprofile = $data.install_profiles[$response].name
-Write-Host "Installing files from profile" -NoNewline
+Write-Host ">> Installing files from profile" -NoNewline
 Write-Host ""$selectedprofile -ForegroundColor Green
-
-
+Write-Host "`r"
 
 # execute commands with selected profiles
 foreach ($app in $data.apps) {
     if ($app.install_profiles -contains $selectedprofile) {        
-        Write-Host "Installing" -NoNewline
+        Write-Host ">> Installing" -NoNewline
         # Write-Host ""$app.file_type -ForegroundColor Green -NoNewline
         Write-Host ""$app.name -ForegroundColor Cyan -NoNewline 
         Write-Host " from" -NoNewline
         Write-Host ""$app.download_source -ForegroundColor Yellow
+        Write-Host "`r"
         # $app.install_powershell_command | Invoke-Expression
     }
 }
+
+Write-Host "All Done! have fun :3" -ForegroundColor Magenta
