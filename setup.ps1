78db81280c336734ae8494b52290eb7e8d32f014
borislaw.sotnikow <borislaw.sotnikow@kuhfussonline.com>
2022-03-09 11:06:38 +0100

    renaming and stuff

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,5 @@
 $json = "./data.json"
+$updateFile = "./update.ps1"
 $data = Get-Content $json | ConvertFrom-Json
 
 # select installation profile
@@ -15,21 +16,33 @@ do {
     $response = $Host.UI.PromptForChoice($title, $msg, $options, $default)
 } until ($response -in 0..($data.install_profiles.Count - 1))
 
-$selectedprofile = $data.install_profiles[$response].name
-Write-Host ">> Installing files from profile" -NoNewline
-Write-Host ""$selectedprofile -ForegroundColor Green
+$selectedprofile = $data.install_profiles[$response]
+Write-Host "> Installing files from profile" -NoNewline
+Write-Host ""$selectedprofile.name -ForegroundColor Green
 Write-Host "`r"
 
-# execute commands with selected profiles
+if ($selectedprofile.generate_update_script) {   
+    if (!(Test-Path -Path $updateFile -PathType Leaf)) {
+        New-Item $updateFile 
+        Write-Host "Created Update File!" -ForegroundColor Yellow
+    }
+    Clear-Content $updateFile
+}
+
 foreach ($app in $data.apps) {
-    if ($app.install_profiles -contains $selectedprofile) {        
+    if ($app.install_profiles -contains $selectedprofile.name) {
+        # install       
         Write-Host ">> Installing" -NoNewline
         # Write-Host ""$app.file_type -ForegroundColor Green -NoNewline
         Write-Host ""$app.name -ForegroundColor Cyan -NoNewline 
         Write-Host " from" -NoNewline
         Write-Host ""$app.download_source -ForegroundColor Yellow
-        Write-Host "`r"
-        $app.install_powershell_command | Invoke-Expression
+
+        $app.powershell_command_install | Invoke-Expression
+
+        if (($app.do_update) -and (Test-Path -Path $updateFile -PathType Leaf)) {                
+            Add-Content $updateFile $app.powershell_command_update
+        }        
     }
 }
 
